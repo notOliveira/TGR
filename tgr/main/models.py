adfb113baad7b9e3b22ed3aa0e4111185d58f8c5
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2023-06-11 00:37:48 -0300

    Modificando base

--- tgr/main/models.py
+++ tgr/main/models.py
@@ -15,7 +15,25 @@ GENRES_OPTIONS = [
     (9, 'Shooter'),
     (10, 'Fighting'),
     (11, 'MOBA'),
-    (12, 'RTS')
+    (12, 'Real-time Strategy'),
+    (13, 'Visual Novel'),
+    (14, 'Terror'),
+    (15, 'Beat \'em up'),
+    (16, 'Platformer'),
+    (17, 'Card game'),
+    (18, 'Board game'),
+    (19, 'Graphic adventure'),
+    (20, 'Stealth'),
+    (21, 'Interactive fiction'),
+    (22, 'Turn-based strategy'),
+    (23, 'Sandbox'),
+    (24, 'Construction game'),
+    (25, 'Roguelike'),
+    (26, 'Metroidvania'),
+    (27, 'Battle royale'),
+    (28, 'Hack and slash'),
+    (29, 'Rhythm game'),
+    (30, 'Educational game'),
     ]
 
 PLATFORMS_OPTIONS = [
