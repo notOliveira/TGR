91ae2655f120eebac72170cbd9c53c4e34f72b84
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2023-06-21 15:13:46 -0300

    Edição de usuário finalizada

--- tgr/users/views.py
+++ tgr/users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, ProfileUpdateForm, UsersUpdateForm
 
 def register_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             nickname = form.cleaned_data['username']
@@ -15,7 +15,7 @@ def register_user(request):
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(
         request, 'users/register.html', {
             'form': form
@@ -44,4 +44,21 @@ def logout_user(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == "POST":
+        user_form = UsersUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Perfil atualizado")
+            return redirect('profile')
+    else:
+        user_form = UsersUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+    
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+    
+    return render(request, 'users/profile.html', context)
