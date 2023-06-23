eecdb81254e3fc249f128f612a8076e181016052
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2023-06-23 11:16:11 -0300

    Recuperação de senha

--- tgr/tgr/settings.py
+++ tgr/tgr/settings.py
@@ -140,3 +140,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+# STPM Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True 
+EMAIL_HOST_USER = 'thegamerecommender@gmail.com'
+EMAIL_HOST_PASSWORD = 'bspyiqnurjgonsga'
--- tgr/users/urls.py
+++ tgr/users/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     path('login/', user_views.login_user, name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
