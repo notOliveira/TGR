88f270cbf954d1d2390b82575c4038acafb36b7b
Gustavo Oliveira <gsantos@nic.br>
2023-06-26 15:53:56 -0300

    Finalizando ciclo de redefinição de senha

--- tgr/users/urls.py
+++ tgr/users/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_sent.html'), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name="password_reset_complete"),
 ]
