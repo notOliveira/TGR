65e0e12baa9e9934a48d6fb39ff1f1f48a6a015f
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2023-06-27 09:24:27 -0300

    Aviso foto de perfil

--- tgr/main/static/main/js/profile.js
+++ tgr/main/static/main/js/profile.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     var textContainer = document.getElementById('upload-pic');
     textContainer.innerHTML = textContainer.innerHTML.replace('Currently: ', '');
-    textContainer.innerHTML = textContainer.innerHTML.replace('Change:', '');
+    textContainer.innerHTML = textContainer.innerHTML.replace('Change:', '');    
 });
