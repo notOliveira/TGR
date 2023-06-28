a912d8103a5dfd8c2261bba5ae815b8ebdf99ef3
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2023-06-28 15:00:38 -0300

    API todos os models

--- tgr/main/views.py
+++ tgr/main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Game, Quote
+from .models import Game, Quote, Platform, Genre
 from rest_framework import viewsets
-from .serializers import GameSerializer
+from .serializers import GameSerializer, QuoteSerializer, PlatformSerializer, GenreSerializer
 # from .forms import GameForm
 # from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -31,3 +31,15 @@ class jogosListView(ListView):
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
+
+class QuoteViewSet(viewsets.ModelViewSet):
+    queryset = Quote.objects.all()
+    serializer_class = QuoteSerializer
+    
+class PlatformViewSet(viewsets.ModelViewSet):
+    queryset = Platform.objects.all()
+    serializer_class = PlatformSerializer
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
