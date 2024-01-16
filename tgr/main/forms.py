b7e8e6980dc009b0aa4561f1b8dd2bfe137f9d9d
Gustavo Oliveira <gsantos@nic.br>
2024-01-16 16:00:50 -0300

    Finalizando adição dos players e adicionando script de criação dos objetos

--- tgr/main/forms.py
+++ tgr/main/forms.py
@@ -5,3 +5,8 @@ class GameForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = '__all__'
+        
+    def __init__(self, *args, **kwargs):
+        super(GameForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
