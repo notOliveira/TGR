f770f28b9d57e64839174ec4583141b9a6483acd
Gustavo Oliveira <gsantos@nic.br>
2024-01-23 13:10:48 -0300

    Corrigindo comentário errado

--- tgr/tgr/settings.py
+++ tgr/tgr/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
-# STPM Configuration
+# SMTP Configuration
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
