90ea52691574617c649889c7a1ae7b4874b70f99
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-01-31 16:52:12 -0300

    Removendo settings_local do repositório

--- tgr/tgr/settings_local.py
+++
@@ -1,5 +0,0 @@
-DATABASE_USER = 'root' # Troque por seu usuário na base de dados
-DATABASE_PASSWORD = 'SUA_SENHA_AQUI' # Troque por sua senha na base de dados
-DATABASE_HOST = 'localhost' # Caso esteja utilizando um servidor remoto, troque para o endereço do servidor
-DATABASE_PORT = '3306' # Troque para a porta utilizada pelo servidor de banco de dados
-SECRET_KEY = '?'
